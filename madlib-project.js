36419c63d60c1b686b93247257447bbd1548f84b
IshaanSingh7 <126361500+IshaanSingh7@users.noreply.github.com>
2023-06-16 11:13:04 -0400

    Add files via upload

---
+++ madlib-project.js
@@ -0,0 +1,7 @@
+let strangeAdjective = 1;
+let strangeAdjective2 = 2;
+let strangeAdjective3 = 3;
+let strangeObject = 4;
+let strangeObject2 = 5;
+let strangeName = 6;
+console.log("Once upon a time a/an " + strangeAdjective + " was playing around with his magical " + strangeObject + " but he accidentally turned it into a/an " + strangeObject2 + ". He was getting alone flying on his " + strangeObject2 + " and wanted to make his own pet named " + strangeName + ". After he created his pet, he and " + strangeName + " decided to fly around together to a castle. The castle was " + strangeAdjective2 + " and " + strangeAdjective3 + ", and they couldn't wait to play around inside. AFter the day was over, the wizard's magical spells wore off and " + strangeName + " disappeared. The End.") 
